30ea264108890c5bfd5aae00077f78fda6ecf845
AriEtemadi <arietemadi>
2019-04-09 23:38:40 +0430

    AIPlayer class added.

---
+++ src/models/AIPlayer.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AIPlayer {
+    private static List<AIPlayer> aiPlayers = new ArrayList<>();
+    private int id;
+
+    private void play() {
+
+    }
+}
