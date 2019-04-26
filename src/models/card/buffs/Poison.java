3e500753f272d050b4e4eb28aec33d3a5c6cbcd1
AriEtemadi <arietemadi>
2019-04-26 14:52:12 +0430

    Poison buff added.

---
+++ src/models/card/buffs/Poison.java
@@ -0,0 +1,26 @@
+package models.card.buffs;
+
+import models.card.ApplyType;
+import models.card.Attacker;
+import models.card.Buff;
+import models.match.Cell;
+
+public class Poison extends Buff {
+    public Poison(int duration, Attacker attacker) {
+        super(duration, null, attacker, ApplyType.ON_ATTACKER);
+    }
+
+    public Poison(int duration, Cell cell) {
+        super(duration, cell, null, ApplyType.ON_CELL);
+    }
+
+    public void apply() {
+        if (duration < 0 || attacker == null && cell == null)
+            return;
+        if (attacker != null)
+            attacker.decreaseHP(1);
+        else if (cell.getAttacker() != null)
+            cell.getAttacker().decreaseHP(1);
+        duration--;
+    }
+}
