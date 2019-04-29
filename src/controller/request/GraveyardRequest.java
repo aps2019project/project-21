4f9bee9760a7f6d3349f3766cecd7f35837d6107
mohammad <sharifim689@gmail.com>
2019-04-29 02:35:40 -0700

    graveyard request completed

--- src/controller/request/GraveyardRequest.java
+++ src/controller/request/GraveyardRequest.java
@@ -1,17 +1,30 @@
 package controller.request;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class GraveyardRequest extends Request {
     @Override
     public void extractType() {
         // set field type in parent class
+        type = RequestType.INVALID;
+        if (commandLine.equals("Show cards"))
+            showCardsCheck();
+        else if (commandLine.startsWith("Show info"))
+            showInfoCheck();
     }
 
-    private boolean showInfoCheck() {
-        return true;
+    private void showInfoCheck() {
+        String regex = "Show info (\\d+)";
+        Matcher matcher = Pattern.compile(regex).matcher(commandLine);
+        if (matcher.matches()){
+            commandArguments.add(matcher.group(1));
+            type = RequestType.SHOW_INFO;
+        }
     }
 
-    private boolean showCardsCheck() {
-        return true;
+    private void showCardsCheck() {
+        type = RequestType.SHOW_CARDS;
     }
 
     protected void backCheck() {
