f36f2a8667562b726a265fb3356e13802108a660
M-Sharifi <sharifikiasari@ce.sharif.edu>
2019-05-06 00:59:38 +0430

    SON, J SON!

--- src/json/CollectableMaker.java
+++ src/json/CollectableMaker.java
@@ -1,9 +1,11 @@
 package json;
 
 import models.Item.Collectable;
+import models.card.ActivationType;
 import models.card.ApplyType;
 import models.card.Effect;
 import models.card.TargetType;
+import models.card.buffs.*;
 import models.card.effects.IncreaseAP;
 import models.card.effects.IncreaseHP;
 import models.card.target_enums.*;
@@ -36,24 +38,63 @@ public class CollectableMaker {
 
 
         // ---------- 3 ----------
-
+        effects = new ArrayList<>();
+        effects.add(new PowerAP(Integer.MAX_VALUE, 3));
+        effects.add(new PowerHP(Integer.MAX_VALUE, 3));
+        targetType = new TargetType(RandomOrNot.RANDOM, TargetAttackerRange.ALL_THREE,
+                CellType.NONE, HeroOrMinion.BOTH, OppOrAlly.ALLY, ChooseType.NONE);
+        collectable = new Collectable("Eksir", targetType, effects,
+                ApplyType.ON_ATTACKER, "3 powerHP and 3 powerAP for random minion.");
+        saveToFile(collectable);
 
         // ---------- 4 ----------
-
+        //later Majone Mana
 
         // ---------- 5 ----------
-
+        effects = new ArrayList<>();
+        for(int i = 0; i < 10; i++)
+            effects.add(new Holy(2));
+        targetType = new TargetType(RandomOrNot.RANDOM, TargetAttackerRange.ALL_THREE,
+                CellType.NONE, HeroOrMinion.MINION, OppOrAlly.ALLY, ChooseType.NONE);
+        collectable = new Collectable("Majone Roein Tani", targetType, effects,
+                ApplyType.ON_ATTACKER, "10 holy buff for a random minion.");
+        saveToFile(collectable);
 
         // ---------- 6 ----------
-
+        effects = new ArrayList<>();
+        effects.add(new WeaknessHP(Integer.MAX_VALUE, 8));
+        effects.get(0).setActivationType(ActivationType.ON_DEATH);
+        targetType = new TargetType(RandomOrNot.NOT_RANDOM, TargetAttackerRange.ALL_THREE,
+                CellType.SQUARE_3_3, HeroOrMinion.BOTH, OppOrAlly.OPP, ChooseType.NONE);
+        collectable = new Collectable("Nefrine Marg", targetType, effects,
+                ApplyType.ON_ATTACKER, "Attack a neighbor opp 8 unit, on death.");
+        saveToFile(collectable);
 
         // ---------- 7 ----------
-
+        effects = new ArrayList<>();
+        effects.add(new WeaknessHP(Integer.MAX_VALUE,2));
+        targetType = new TargetType(RandomOrNot.RANDOM, TargetAttackerRange.ALL_THREE,
+                CellType.NONE, HeroOrMinion.BOTH, OppOrAlly.OPP, ChooseType.NONE);
+        collectable = new Collectable("Random Damage", targetType, effects,
+                ApplyType.ON_ATTACKER, "Damage an attacker 2 units.");
+        saveToFile(collectable);
 
         // ---------- 8 ----------
-
+        effects = new ArrayList<>();
+        effects.add(new PowerAP(Integer.MAX_VALUE, 6));
+        targetType = new TargetType(RandomOrNot.RANDOM, TargetAttackerRange.ALL_THREE,
+                CellType.NONE, HeroOrMinion.BOTH, OppOrAlly.ALLY, ChooseType.NONE);
+        collectable = new Collectable("Blades of agility", targetType, effects,
+                ApplyType.ON_ATTACKER, "PowerAp 6 units for a random ally.");
+        saveToFile(collectable);
 
         // ---------- 9 ----------
-
+        effects = new ArrayList<>();
+        effects.add(new PowerAP(Integer.MAX_VALUE,5));
+        targetType = new TargetType(RandomOrNot.NOT_RANDOM, TargetAttackerRange.MELEE,
+                CellType.NONE, HeroOrMinion.BOTH, OppOrAlly.ALLY, ChooseType.NONE);
+        collectable = new Collectable("Shamshire Chini", targetType, effects,
+                ApplyType.ON_ATTACKER, "PowerAP 5 units for melee ally.");
+        saveToFile(collectable);
     }
 }
