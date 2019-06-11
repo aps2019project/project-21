90f542c81fc6f19b6730b135473dddfa4fd2aea3
mohammad <sharifim689@gmail.com>
2019-06-11 10:02:52 -0700

    Attacker show handled

--- src/view/CardView.java
+++ src/view/CardView.java
@@ -6,11 +6,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import models.card.Card;
-import models.card.Hero;
-import models.card.Minion;
-import models.card.Spell;
+import models.card.*;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -25,38 +23,57 @@ class CardView {
 
 
     static Group shopCardGroup(Card card) {
-        Group ret = new Group();
-        try {
-            ImageView imageView = new ImageView(new Image(new FileInputStream
-                    ("C:\\project-21\\src\\assets\\cards\\shop_background.png")));
+        if (card instanceof Attacker) {
+            Group ret = new Group();
+            try {
+                ImageView imageView = new ImageView(new Image(new FileInputStream
+                        ("C:\\project-21\\src\\assets\\cards\\shop_background.png")));
 
-            ImageView image;
-            if (card instanceof Hero) {
-                image = new ImageView(new Image(new FileInputStream
-                        ("C:\\project-21\\src\\assets\\cards\\hero\\" + card.getName() + ".png")));
-            } else if (card instanceof Minion) {
-                image = new ImageView(new Image(new FileInputStream
-                        ("C:\\project-21\\src\\assets\\cards\\minion\\" + card.getName() + ".png")));
-            } else if (card instanceof Spell) {
-                image = new ImageView(new Image(new FileInputStream
-                        ("C:\\project-21\\src\\assets\\cards\\spell\\" + card.getName() + ".png")));
-            } else {
-                image = new ImageView(new Image(new FileInputStream
-                        ("C:\\project-21\\src\\assets\\cards\\usable\\" + card.getName() + ".png")));
+                ImageView image;
+                if (card instanceof Hero) {
+                    image = new ImageView(new Image(new FileInputStream
+                            ("C:\\project-21\\src\\assets\\cards\\hero\\" + card.getName() + ".png")));
+                } else {
+                    image = new ImageView(new Image(new FileInputStream
+                            ("C:\\project-21\\src\\assets\\cards\\minion\\" + card.getName() + ".png")));
+                }
+                image.relocate(0, -50);
+
+                ImageView mp = new ImageView(new Image(new FileInputStream
+                        ("C:\\project-21\\src\\assets\\cards\\mana.png")));
+                Label MP = new Label(Integer.toString(card.getPrice()));
+                MP.setFont(Font.font(14));
+                MP.relocate(7, 22);
+
+                Label hp = new Label("");
+                hp.relocate(167, 170);
+                hp.setTextFill(Color.WHITE);
+                hp.setFont(Font.font(14));
+                hp.setText(Integer.toString(((Attacker) card).getHP()));
+
+
+                Label ap = new Label(Integer.toString(((Attacker) card).getAP()));
+                ap.relocate(55,170);
+                ap.setTextFill(Color.WHITE);
+                ap.setFont(Font.font(14));
+
+                Image t = new Image(new FileInputStream("C:\\project-21\\src\\assets\\cards\\name.png"));
+                ImageView nameImage = new ImageView(t);
+                nameImage.setScaleX(0.45);
+                nameImage.relocate(28,125);
+
+                Label name = new Label(card.getName() + "\n  " + ((card instanceof Hero)?"Hero":"Minion"));
+                name.relocate(87,135);
+                name.setTextFill(Color.WHITE);
+                name.setFont(Font.font(14));
+
+                ret.getChildren().addAll(imageView, image, nameImage, mp, MP, hp, ap, name);
+                return ret;
+            } catch (IOException ex) {
+                return null;
             }
-            image.relocate(0, -50);
-
-            ImageView mp = new ImageView(new Image(new FileInputStream
-                    ("C:\\project-21\\src\\assets\\cards\\mana.png")));
-            Label MP = new Label(Integer.toString(card.getPrice()));
-            MP.setFont(Font.font(14));
-            MP.relocate(7, 22);
-
-            ret.getChildren().addAll(imageView, image, mp, MP);
-            return ret;
-        } catch (IOException ex) {
-            return null;
         }
+        return null;
     }
 
 }
