03d4fd64e244bb382903f66a8440614c66abe127
M-Sharifi <sharifikiasari@ce.sharif.edu>
2019-06-28 22:12:30 +0430

    debug in view

--- src/view/MatchHistoryView.java
+++ src/view/MatchHistoryView.java
@@ -59,18 +59,6 @@ class MatchHistoryView {
         handleButtons();
     }
 
-    private void setBackground() {
-        try {
-            ImageView background = new ImageView(new Image(new FileInputStream("src\\assets\\MatchHistory" +
-                    "\\background.jpg")));
-            background.fitWidthProperty().bind(scene.widthProperty());
-            background.fitHeightProperty().bind(scene.heightProperty());
-            root.getChildren().add(background);
-        } catch (Exception e) {
-            View.printThrowable(e);
-        }
-    }
-
     private void draw() {
         root.getChildren().addAll(volume);
         
